ec7fd1058333d35fbb7799d0a9e9de9861601859
ShahzebMalik107 <33540322+ShahzebMalik107@users.noreply.github.com>
2021-04-06 18:38:52 +0500

    Tabs active done

--- Webpages/Burger streat/main.js
+++ Webpages/Burger streat/main.js
@@ -4,6 +4,56 @@ function ListActive(tst)
     console.log(tst);
     if(tst=='t1')
     {
-        
+        t1.classList.add('tabs--active')
+        t2.classList.remove('tabs--active');
+        t3.classList.remove('tabs--active');
+        t4.classList.remove('tabs--active');
+        t5.classList.remove('tabs--active');
+        t6.classList.remove('tabs--active');
+    }
+    if(tst=='t2')
+    {
+        t2.classList.add('tabs--active')
+        t1.classList.remove('tabs--active');
+        t3.classList.remove('tabs--active');
+        t4.classList.remove('tabs--active');
+        t5.classList.remove('tabs--active');
+        t6.classList.remove('tabs--active');
+    }
+    if(tst=='t3')
+    {
+        t3.classList.add('tabs--active')
+        t2.classList.remove('tabs--active');
+        t1.classList.remove('tabs--active');
+        t4.classList.remove('tabs--active');
+        t5.classList.remove('tabs--active');
+        t6.classList.remove('tabs--active');
+    }
+    if(tst=='t4')
+    {
+        t4.classList.add('tabs--active')
+        t2.classList.remove('tabs--active');
+        t3.classList.remove('tabs--active');
+        t1.classList.remove('tabs--active');
+        t5.classList.remove('tabs--active');
+        t6.classList.remove('tabs--active');
+    }
+    if(tst=='t5')
+    {
+        t5.classList.add('tabs--active')
+        t2.classList.remove('tabs--active');
+        t3.classList.remove('tabs--active');
+        t4.classList.remove('tabs--active');
+        t1.classList.remove('tabs--active');
+        t6.classList.remove('tabs--active');
+    }
+    if(tst=='t6')
+    {
+        t6.classList.add('tabs--active')
+        t2.classList.remove('tabs--active');
+        t3.classList.remove('tabs--active');
+        t4.classList.remove('tabs--active');
+        t5.classList.remove('tabs--active');
+        t1.classList.remove('tabs--active');
     }
 }
